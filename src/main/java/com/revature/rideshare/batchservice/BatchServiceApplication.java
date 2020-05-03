778b63e640ab0993f919fc2253de6ded51f5ea15
Gilbert Turner <grturner@5x5code.com>
2020-05-03 18:18:19 -0400

    Logging Added AOP Logging

--- src/main/java/com/revature/rideshare/batchservice/BatchServiceApplication.java
+++ src/main/java/com/revature/rideshare/batchservice/BatchServiceApplication.java
@@ -2,14 +2,18 @@ package com.revature.rideshare.batchservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class BatchserviceApplication {
+@ServletComponentScan
+@EnableAspectJAutoProxy
+public class BatchServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BatchserviceApplication.class, args);
+		SpringApplication.run(BatchServiceApplication.class, args);
 	}
 
 }
